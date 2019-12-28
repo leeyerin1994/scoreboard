859f3a611fe42bf0187b7361805302aed62e11ad
AhnJunsik <nyupe@naver.com>
2019-12-28 14:04:38 +0900

    191228 add

---
+++ src/components/AddPlayerForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class AddPlayerForm extends React.Component {
+  render() {
+    return (
+      <div>
+        AddPlayerForm works.
+      </div>
+    );
+  }
+}
---
+++ src/components/Stopwatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Stopwatch extends React.Component {
+  render() {
+    return (
+      <div>
+        Stopwatch works.
+      </div>
+    );
+  }
+}
